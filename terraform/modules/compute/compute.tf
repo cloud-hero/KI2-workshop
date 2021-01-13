c3c44273e41509b8114ce82c37f21e6fb8ada982
Andrei Petrescu <andrei.petrescu@cloudhero.io>
2021-01-13 11:08:25 +0200

    first push

---
+++ terraform/modules/compute/compute.tf
@@ -0,0 +1,27 @@
+resource "google_compute_instance" "node" {
+  name            = var.name
+  machine_type    = var.machine_type
+  zone            = var.zone
+  can_ip_forward  = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-lts"
+      size  = "100"
+    }
+  }
+
+
+  network_interface {
+    network     = var.network
+    subnetwork  = var.subnetwork
+    access_config {}
+    network_ip  = var.network_ip
+  }
+
+  metadata_startup_script = var.metadata_startup_script
+
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+}
